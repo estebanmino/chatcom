56db65c9ab3310815ef1532bfa3e4b55ebba780c
esteban <efmino@uc.cl>
2018-03-16 20:42:18 -0700

    enabling functionalities fb

--- index.js
+++ index.js
@@ -19,6 +19,38 @@ app.get('/', function(req, res) {
 
 // facebook
 
+app.post('/webhook/', function(req, res) {
+    let messaging_events= req.body.entry[0].messaging_events
+    for (let i =0; i < messaging_events.length; i++) {
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendText(sender, "Text: " + text.substring(0,100))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+    let messageData = {text: text}
+    request({
+        url: "https://graph.facebook.com/v2.12/me/messages",
+        qs: {access_token, TOKEN },
+        method: "POST",
+        json: {
+            receipt:  {id: sender},
+            message: messageData
+        }, function(error, response, body) {
+            if (error) {
+                console.log('sending error')
+            } else if (response.body.error) {
+                console.log("response body error")
+            }
+        }
+    })
+}
+
 app.get('/webhook/', function(req, res) {
     if (req.query['hub.verify_token'] === TOKEN) {
         res.send(req.query['hub.challenge'])
